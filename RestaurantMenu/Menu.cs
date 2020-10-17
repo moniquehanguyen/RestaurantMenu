f40741c50aef434f0b896e321393bd57a35d2917
Monique Nguyen <moniquehanguyen@gmail.com>
2020-10-17 10:09:14 -0500

    Add project files.

---
+++ RestaurantMenu/Menu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantMenu
+{
+    class Menu
+    {
+        public List<MenuItem> Appetizer;
+        public List<MenuItem> Entree;
+        public List<MenuItem> Dessert;
+        public List<MenuItem> NewItems;
+        public DateTime DateUpdated;
+    }
+}
---
+++ RestaurantMenu/MenuItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantMenu
+{
+    public class MenuItem
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Category { get; set; }
+        private DateTime dateAdded;
+        public DateTime DateAdded
+        {
+            get { return dateAdded; }
+            set { dateAdded = value; }
+        }
+
+        public MenuItem(string name, double price, string category, DateTime dateAdded)
+        {
+            Name = name;
+            Price = price;
+            Category = category;
+            DateAdded = dateAdded;
+        }
+    }
+}
